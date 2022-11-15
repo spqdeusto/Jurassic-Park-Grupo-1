64784b4e39100c550abf0c00eae36badc40a8e5d
rubenclaveras <rubenclaveras@opendeusto.es>
2022-11-15 13:39:38 +0100

    Error not found routes

---
+++ README.md
@@ -0,0 +1,9 @@
+# Jurassic-Park-Grupo-1
+
+Grupo 1 - Rubén Claveras, Gorka Esteban y Lorea Intxausti. 
+Las tecnologías a utilizar son las siguientes.
+API de conexión: FastApi.
+ORM: SQLAlchemy.
+Base de Datos: MySQL
+Frontend: Flutter programado en Dart.
+Levantaremos contenedores con Docker.
---
+++ backend/models/dinosaur.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Table, Column
+from sqlalchemy.sql.sqltypes import Integer, String, Float, Boolean
+from config.db import meta, engine
+
+dinosaurs = Table("dinosaurs", meta, 
+    Column("id", Integer, primary_key=True), 
+    Column("name", String(255)), 
+    Column("species", String(255)), 
+    Column("age", Integer), 
+    Column("weight", Float), 
+    Column("gender", String(255)), 
+    Column("dangerousness", Boolean))
+
+meta.create_all(engine)
