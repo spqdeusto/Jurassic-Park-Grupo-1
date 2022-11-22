1375e0f02684eda57850c4ab2cb90702590a83e3
Gorka124 <estebanareizaga@opendeusto.es>
2022-11-22 11:34:04 +0100

    Ser o no ser Docker

--- backend/config/db.py
+++ backend/config/db.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine, MetaData
+from sqlalchemy_utils import database_exists, create_database
 
-engine = create_engine("mysql+pymysql://root:example@localhost:3306/mysql_jurasic")
+engine = create_engine("mysql+pymysql://root:example@mysql-fastapi:3306/mysql_jurasic")
+
+if not database_exists(engine.url):
+    create_database(engine.url)
 
 meta = MetaData()
 
